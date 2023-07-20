64da7963914ba4951b3b42eb1edf2fe48e99b53a
Lizzie Casaleno <lizziecasaleno1999@gmail.com>
2023-07-20 10:21:37 -0400

    Initial Commit

---
+++ donatiePage/donatiePageApp.swift
@@ -0,0 +1,17 @@
+//
+//  donatiePageApp.swift
+//  donatiePage
+//
+//  Created by scholar on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct donatiePageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
